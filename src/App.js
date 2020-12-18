78ccf7d9398c152c653762ab0f1141da83f0f109
Lev Michael <lev.choubine@gmail.com>
2020-12-18 15:38:16 -0500

    handle log-out

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import './App.css';
-import Welcome from './Components/Welcome';
+import React, {useEffect, useState} from 'react';
+import {Route, Switch, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode'
+import setAuthToken from './utils/setAuthToken'
 
+import './App.css';
+
+import Welcome from './Components/Welcome';
+import Navbar from './Components/Navbar'
+import { Component } from 'react';
+
+
+
+const PrivateRoute =({component: Component, ...rest})=>{
+const user = localStorage.getIem('jwtToken');
+return <Route {...rest} render={(props)=>{
+return user ? <Component { ...rest }/> : <Redirect to="/login" />
+}}/>
+}
+
+ 
 function App() {
+const[currentUser, setCurrentUser] = useState('');
+const[isAuthenticated, setIsAuthenticated]=useState(true);
+useEffect(()=>{
+  let token;
+  if(!localStorage.getItem('jwtToken')){
+    setIsAuthenticated(false);
+  }else{
+    token = jwt_decode(localStorage.getItem('jwtToken'));
+    setAuthToken(localStorage.jwtToken)
+    setCurrentUser(token)
+  }
+}, [])
+
+const nowCurrentUser = (userData) => {
+  console.log('nowCurrebtUser is here...');
+  setCurrentUser(userData);
+  setIsAuthenticated(true);
+}
+
+const handleLogout = () => {
+  if(locatStorage.getItem('jwtToken')){
+    localStorage.removeItem('jwtToken')
+    setCurrrentUser(null)
+    setIsAuthenticated(false);
+  }
+}
+
   return (
     <div className="App">
+      <Navbar handleLogout={handleLogout} isAuth={isAuthenticated}/>
     <Welcome />
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,50 @@
-import Navbar from 'react'
+import React from 'react';
+import {NavLink, Link} from 'react-router-dom';
 
 const Navbar =()=>{
     return(
-     <footer className="footer bg-dark">
-         <div className="container text-center">
-             <span className="text-muted"> @2020 built by Tech Gods (in times of great trouble)</span>
-         </div>
+    <nav className="navbar navbar-expand-lg navbar">
+       <div className="container">
+                <Link className="navbar-brand" to="/">MERN Auth</Link>
+                <button className="navbar-toggler" type="button" data-toggle="#navbarsExample07" data-target="navbarsExample07" aria-controls="navbarsExample07" aria-label="Toggle Navigation">
+                    <span className="navbar-toogler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarExample07">
+                    <ul className= "navbar-nav mr-auto">
+                        <li className="collapse navbar-collapse" id="navbarsExample07">
+                            <NavLink className="nav-link" exact to="/">Home</NavLink>
+                        </li>
+                        <li className="collapse navbar-collapse" id="navbarsExample07">
+                            <NavLink className="nav-link" exact to="/about">About</NavLink>
+                        </li>
 
-     </footer>
+                    </ul>
+                     {
+                         props.isAuth ?
+                         <ul className= "navbar-nav ml-auto">
+                        <li className="collapse navbar-collapse" id="navbarsExample07">
+                            <NavLink className="nav-link" to="/profile">Profile</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <span onClick={props.handleLogout} className="nav-link logout-link">Logout</span>
+                        </li>
+
+                    </ul>
+                      :
+                      <ul className= "navbar-nav ml-auto">
+                        <li className="collapse navbar-collapse" >
+                            <NavLink className="nav-link"  to="/signup">Create Account</NavLink>
+                        </li>
+                        <li className="nav-link" >
+                            <NavLink className="nav-link" to="/login">Login</NavLink>
+                        </li>
+
+                    </ul>
+                     }
+
+                </div>
+            </div>
+    </nav>
     )
 }
 
